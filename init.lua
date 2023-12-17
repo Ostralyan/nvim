b81e292d3612caad7d46809c62cb56ab2a4054a7
Luke Xu <lukecxu@gmail.com>
2023-12-17 16:48:57 -0500

    Added a bunch of extra things

--- init.lua
+++ init.lua
@@ -8,11 +8,10 @@ vim.bo.tabstop = 2 -- set tabstop=2
 vim.bo.expandtab = true -- set expandtab
 vim.wo.cursorline = true -- set cursorline
 vim.g.termguicolors = true -- set termguicolors
+vim.g.mapleader = " " -- set mapleader to <Space>
 -- set mouse=a
 -- filetype indent plugin on
 -- set noswapfile
-
--- vim.g.mapleader = " "
 
 -- Bootstraps lazy.nvim package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -31,5 +30,45 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins") -- looks for plugins in ~/config/nvim/lua/plugins.lua
 -- End of lazy.nvim initialization
 
+-- Loads mason which installs LSPs
+require("mason").setup()
+
 -- Loads everforest colorscheme
 require("everforest").load()
+
+-- Loads nvim tree file explorer
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- set up for nvim-tree
+require("nvim-tree").setup()
+
+-- buffer line options
+require('bufferline').setup({
+  options = {
+    mode = "buffers", -- set to "tabs" to only show tabpages instead
+    numbers = "buffer_id",
+    separator_style = "thick",
+    show_buffer_close_icons = false,
+    show_buffer_default_icon = false, -- whether or not an unrecognised filetype should show a default icon
+  }
+})
+
+require("ibl").setup()
+
+-- Key Mappings
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Nvim Tree
+vim.keymap.set('n', '<leader>zz', "<cmd>NvimTreeToggle<cr>")
+vim.keymap.set('n', '<leader>zf', "<cmd>NvimTreeFindFile<cr>")
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,20 +8,60 @@ return {
     },
     -- lualine nvm status line 
     {
-      'nvim-lualine/lualine.nvim',
+      "nvim-lualine/lualine.nvim",
       config = function()
         -- Loads lualine nvim status line
         require("lualine").setup({
-	  options = {
-	    theme = "everforest",
-	  }})
+        options = {
+          theme = "everforest",
+        }})
       end
     },
     -- comment nvim
     {
-      'numToStr/Comment.nvim',
+      "numToStr/Comment.nvim",
       config = function()
-        require('Comment').setup()
+        require("Comment").setup()
       end
+    },
+    -- configuring the nvim lsp
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+      	require("lspconfig").tsserver.setup({})
+      end
+    },
+    -- installing mason to manage lsps
+    {
+      "williamboman/mason.nvim"
+    },
+    -- adds telescope file search using fzf
+    {
+      "nvim-telescope/telescope.nvim", tag = "0.1.5",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require("telescope").setup({})
+        require("telescope").load_extension('fzf')
+      end
+    },
+    { 
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    },
+    {
+      "nvim-tree/nvim-tree.lua",
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate"
+    },
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      main = "ibl",
+    },
+    {
+      'akinsho/bufferline.nvim',
+      version = "*",
+      dependencies = 'nvim-tree/nvim-web-devicons'
     }
 }
